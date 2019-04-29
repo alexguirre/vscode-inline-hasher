495f7799e9e8dc27c145bef2d59314b4ce092f0d
alexguirre <alexguirre.aa@gmail.com>
2019-04-29 20:40:13 +0200

    Implement joaat commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,48 @@
 import * as vscode from 'vscode';
+import * as hash from './hash-functions';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.test', () => {
-		// The code you place here will be executed every time your command is executed
+	const commandList = [
+		vscode.commands.registerCommand('inlineHash.joaat', joaatCallback),
+		vscode.commands.registerCommand('inlineHash.joaatLowerCase', joaatLowerCaseCallback),
+		vscode.commands.registerCommand('inlineHash.joaatUpperCase', joaatUpperCaseCallback),
+	]
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Test!');
-	});
-
-	context.subscriptions.push(disposable);
+	commandList.forEach(d => context.subscriptions.push(d));
 }
 
-export function deactivate() {}
+export function deactivate() { }
+
+
+const joaatCallback = () => hashCallback(hash.joaat);
+const joaatLowerCaseCallback = () => hashCallback(hash.joaatLowerCase);
+const joaatUpperCaseCallback = () => hashCallback(hash.joaatUpperCase);
+
+type HashFunction = (str: string) => number;
+function hashCallback(hashFunc: HashFunction) {
+	const textEditor = vscode.window.activeTextEditor;
+	if (!textEditor) {
+		return;
+	}
+	
+	const selection = textEditor.selection;
+	if (selection.isEmpty) {
+		return;
+	}
+
+	const lineStart = selection.start.line;
+	const lineEnd = selection.end.line;
+	const newLines: string[] = []
+	for (let i = lineStart; i <= lineEnd; i++) {
+		const line = textEditor.document.lineAt(i).text;
+
+		const newLine = line + " = 0x" + hashFunc(line).toString(16).toUpperCase();
+		newLines.push(newLine);
+	}
+
+	textEditor.edit(editBuilder => {
+		const range = new vscode.Range(lineStart, 0, lineEnd, textEditor.document.lineAt(lineEnd).text.length);
+		editBuilder.replace(range, newLines.join('\n'));
+	});
+}
